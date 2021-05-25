86d40bd32047857c85aae3cb3ab76bbf5d432b59
hedin <hedinant@gmail.com>
2021-05-25 21:40:12 +0300

    react initial

---
+++ ui/src/Login/index.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./index.css";
+
+async function loginUser(username, password) {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((data) => "token" /*data.json()*/)
+    .catch((reason) => "server not responded");
+}
+
+export default function Login({ setToken }) {
+  console.log("Login in " + setToken);
+
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser(username, password);
+    console.log("submit " + setToken);
+    setToken(token);
+  };
+
+  return (
+    <>
+      <a className="Test" href="">
+        Регистрация
+      </a>
+      <div className="LoginForm">
+        <form onSubmit={handleSubmit}>
+          <div className="Username">
+            <span>Username</span>
+            <input
+              className="UsernameInput"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <label className="">
+            <span>Password</span>
+            <input
+              className="PasswordInput"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>{" "}
+          <div className="Example">
+            <button type="submit" name="login">
+              OK
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
